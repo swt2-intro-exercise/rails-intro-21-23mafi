194fdfe16050d2951f4e2aa3a4954941ab541820
Marie Fischer <marie.fischer@student.hpi.uni-potsdam.de>
2021-11-09 11:21:56 +0100

    add model for authors and migrate db

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  author = Author.new()
-  author.first_name = "Alan"
-  author.last_name = "Turing"
-  author.homepage = "http://wikipedia.org/Alan_Turing"
-  expect(author.first_name).to eq("Alan")
-  expect(author.last_name).to eq("Turing")
-  expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
+  it "should determine first_name, last_name and hompage from an author" do
+    author = Author.new()
+    author.first_name = "Alan"
+    author.last_name = "Turing"
+    author.homepage = "http://wikipedia.org/Alan_Turing"
+    expect(author.first_name).to eq("Alan")
+    expect(author.last_name).to eq("Turing")
+    expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
+  end
 end
