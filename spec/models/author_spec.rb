e713ed6b74853712de1f07b085316287ae615508
Marie Fischer <marie.fischer@student.hpi.uni-potsdam.de>
2021-11-10 14:16:40 +0100

    Failing test for #9

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -10,11 +10,18 @@ RSpec.describe Author, type: :model do
     expect(author.last_name).to eq("Turing")
     expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
   end
-  it "should determine first_name and last_name from name" do
-  a = Author.new()
-  a.first_name = "Alan"
-  a.last_name = "Turing"
-  a.homepage = "http://wikipedia.org/Alan_Turing"
-  expect(a.name).to eq("Alan Turing")
-end
+    it "should determine first_name and last_name from name" do
+    author = Author.new()
+    author.first_name = "Alan"
+    author.last_name = "Turing"
+    author.homepage = "http://wikipedia.org/Alan_Turing"
+    expect(author.name).to eq("Alan Turing")
+  end
+  it "should be that last_name of an author is not empty" do
+    author = Author.new()
+    author.first_name = "Alan"
+    author.last_name = "Turing"
+    author.homepage = "http://wikipedia.org/Alan_Turing"
+    expect(author).to_not be_valid
+  end
 end
