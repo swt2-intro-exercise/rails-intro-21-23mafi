5defdcf540bac62e04fea6c03f7b9448fbd753e8
Marie Fischer <marie.fischer@student.hpi.uni-potsdam.de>
2021-11-16 23:42:57 +0100

    add validation for papers and tests

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,2 +1,9 @@
 class Paper < ApplicationRecord
+    validates :title, presence: true,
+                    length: { minimum: 1 }
+    validates :venue, presence: true,
+                    length: { minimum: 1 }
+    validates :year, presence: true,
+                    length: { minimum: 1 },
+                    numericality: { only_integer: true }
 end
--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -1,24 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe "papers/new", type: :view do
-  before(:each) do
-    assign(:paper, Paper.new(
-      title: "MyString",
-      venue: "MyString",
-      year: 1
-    ))
+RSpec.describe "papers/new", type: :feature do
+  it "should exist at 'new_paper_path' and render withour error" do
+    # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
+    visit new_paper_path
   end
-  
-  it "renders new paper form" do
-    render
+  it "should have text inputs for an paper's title, venue, and year" do
+    visit new_paper_path
+    # these are the standard names given to inputs by the Rails form builder
+    expect(page).to have_field('paper[title]')
+    expect(page).to have_field('paper[venue]')
+    expect(page).to have_field('paper[year]')
+  end
+  it "should create and submit a new paper" do
+    visit new_paper_path
+    page.fill_in 'paper[title]', with: 'Alans biogriphy '
+    page.fill_in 'paper[venue]', with: 'Berlin'
+    page.fill_in 'paper[year]', with: '2000'
+    find('input[type="submit"]').click
+  end
 
-    assert_select "form[action=?][method=?]", papers_path, "post" do
+  it "should not submit an invalid new page" do
+    visit new_paper_path
+    page.fill_in 'paper[title]', with: ''
+    page.fill_in 'paper[venue]', with: 'Berlin'
+    page.fill_in 'paper[year]', with: '1'
+    find('input[type="submit"]').click
+    expect(page).to have_text('error')
+    expect(page).to have_text('Title can\'t be blank')
+  end
 
-      assert_select "input[name=?]", "paper[title]"
-
-      assert_select "input[name=?]", "paper[venue]"
-
-      assert_select "input[name=?]", "paper[year]"
-    end
+  it "should not submit another invalid new page" do
+    visit new_paper_path
+    page.fill_in 'paper[title]', with: 'Test'
+    page.fill_in 'paper[venue]', with: 'Berlin'
+    page.fill_in 'paper[year]', with: 'w'
+    find('input[type="submit"]').click
+    expect(page).to have_text('error')
+    expect(page).to have_text('Year is not a number')
   end
 end
