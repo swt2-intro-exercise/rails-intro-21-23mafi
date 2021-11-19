efdb07131491ad1cb80e5dd9b326c8092794151c
Marie Fischer <marie.fischer@student.hpi.uni-potsdam.de>
2021-11-19 15:18:37 +0100

    failing test #17

--- spec/features/paper/show_paper_spec.rb
+++ spec/features/paper/show_paper_spec.rb
@@ -1,18 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "papers/show", type: :view do
-  before(:each) do
-    @paper = assign(:paper, Paper.create!(
-      title: "Title",
-      venue: "Venue",
-      year: 2
-    ))
+RSpec.describe "papers/show", type: :feature do
+  it "renders attributes in <p>" do
+    @paper = Paper.create!(
+        title: "Title",
+        venue: "Venue",
+        year: 2
+      )
+    visit paper_path(@paper)
+    expect(page).to have_text("Title")
+    expect(page).to have_text("Venue")
+    expect(page).to have_text("2")
+  end
+  it "should see the full name of an author" do
+    @paper = FactoryBot.create :paper
+
+    visit paper_path(@paper)
+    expect(page).to have_text("COMPUTING MACHINERY AND INTELLIGENCE")
+    expect(page).to have_text("Mind 49: 433-460")
+    expect(page).to have_text("1950")
+    expect(page).to have_text("Alan")
+    expect(page).to have_text("Turing")
   end
 
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Title/)
-    expect(rendered).to match(/Venue/)
-    expect(rendered).to match(/2/)
-  end
 end
