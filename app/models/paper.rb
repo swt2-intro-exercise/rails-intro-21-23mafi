f74460ed6c0f9db0808f1ea83197db8468c6b29d
Marie Fischer <marie.fischer@student.hpi.uni-potsdam.de>
2021-11-19 16:30:54 +0100

    add filter to paper index page

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -7,4 +7,5 @@ class Paper < ApplicationRecord
                     length: { minimum: 1 },
                     numericality: { only_integer: true }
     has_and_belongs_to_many :authors
+    scope :year, -> (time) { where("year = ?", time) }
 end
